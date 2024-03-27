438bf453d8166d174acb25377a1b67b0c51026c3
masproo22 <m.abbaszadeh2002@gmail.com>
2024-03-27 12:14:38 +0330

    fix scroller component

--- components/modules/scroller.js
+++ components/modules/scroller.js
@@ -1,22 +1,32 @@
-"use client"
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 
 export default function Scroller() {
+  const [scrollPosition, setScrollPosition] = useState(0);
 
-  window.addEventListener("scroll", (e) => {
-    
-  })
+  useEffect(() => {
+    const updatePosition = () => {
+      setScrollPosition(window.scrollY);
+    };
 
-  return (
-    <div
-    onClick={() => scrollTo(0, 0)}
-    className="active:scale-95 bg-[#9ce89d] w-[4rem] h-[4rem] rounded-[50%] z-10 fixed bottom-[10rem] right-4 flex items-center justify-center"
-  >
-    <img
-      className="w-[3rem] h-[3rem]"
-      src="/images/arrow-up.svg"
-      alt="arrow-up"
-    />
-  </div>
-  );
+    window.addEventListener("scroll", updatePosition);
+
+    // Clean up the event listener when the component unmounts
+    return () => window.removeEventListener("scroll", updatePosition);
+  }, []);
+
+  return scrollPosition > 1000 ? (
+    <>
+      <div
+        onClick={() => scrollTo(0, 0)}
+        className="active:scale-95 bg-[#9ce89d] w-[4rem] h-[4rem] rounded-[50%] z-10 fixed bottom-[10rem] right-4 flex items-center justify-center"
+      >
+        <img
+          className="w-[3rem] h-[3rem]"
+          src="/images/arrow-up.svg"
+          alt="arrow-up"
+        />
+      </div>
+    </>
+  ) : null;
 }
