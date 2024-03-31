62e7fd4e9553869fb1761cc5f0001a3cd7f041b0
masproo22 <m.abbaszadeh2002@gmail.com>
2024-03-31 12:09:50 +0330

    comments page

--- src/app/comments/page.js
+++ src/app/comments/page.js
@@ -1,7 +1,17 @@
 import React from 'react'
+import Comment from '../../../components/modules/comment'
 
 export default function Comments() {
   return (
-    <div className='w-full h-screen'></div>
+    <div className='w-full p-12 pb-[15rem] flex flex-col gap-8 items-center'>
+      <Comment/>
+      <Comment/>
+      <Comment/>
+      <Comment/>
+      <Comment/>
+      <Comment/>
+      <Comment/>
+      <Comment/>
+    </div>
   )
 }
