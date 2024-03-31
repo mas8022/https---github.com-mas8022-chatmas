4c354fb76edd7060bcfef773fa2ccb79d505e4b9
masproo22 <m.abbaszadeh2002@gmail.com>
2024-03-31 12:13:47 +0330

    stories page

--- src/app/stories/page.js
+++ src/app/stories/page.js
@@ -1,7 +1,14 @@
 import React from 'react'
+import Timeline from '../../../components/modules/timeline'
 
 export default function Stories() {
   return (
-    <div className='w-full h-screen'></div>
+    <div className='w-full p-12 pb-[14rem] flex flex-col gap-8 items-center'>
+      <Timeline/>
+      <Timeline/>
+      <Timeline/>
+      <Timeline/>
+      <Timeline/>
+    </div>
   )
 }
