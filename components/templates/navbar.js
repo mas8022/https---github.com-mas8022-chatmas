279b043e8208efda343c87536de1afc596f6f961
masproo22 <m.abbaszadeh2002@gmail.com>
2024-04-15 10:34:08 +0330

    fix

--- components/templates/navbar.js
+++ components/templates/navbar.js
@@ -61,7 +61,7 @@ export default function Navbar({ userId }) {
 
       <div className={flagSide ? "sideActive" : "sideDeActive"}>
         <div className="w-full flex flex-col gap-5">
-          <Link href={`/posts/${userId}`}>
+          <Link href={"/postUpload"}>
             <div className="sideItemDeActive active:sideItemActive ">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
@@ -78,10 +78,10 @@ export default function Navbar({ userId }) {
                 />
               </svg>
 
-              <span className="font-medium text-[1.7rem]">Posts</span>
+              <span className="font-medium text-[1.7rem]">Post</span>
             </div>
           </Link>
-          <Link href={"/comments"}>
+          <Link href={`/posts/${userId}`}>
             <div className="sideItemDeActive active:sideItemActive">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
@@ -94,14 +94,14 @@ export default function Navbar({ userId }) {
                 <path
                   strokeLinecap="round"
                   strokeLinejoin="round"
-                  d="M7.5 8.25h9m-9 3H12m-9.75 1.51c0 1.6 1.123 2.994 2.707 3.227 1.129.166 2.27.293 3.423.379.35.026.67.21.865.501L12 21l2.755-4.133a1.14 1.14 0 0 1 .865-.501 48.172 48.172 0 0 0 3.423-.379c1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0 0 12 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018Z"
+                  d="m2.25 15.75 5.159-5.159a2.25 2.25 0 0 1 3.182 0l5.159 5.159m-1.5-1.5 1.409-1.409a2.25 2.25 0 0 1 3.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 0 0 1.5-1.5V6a1.5 1.5 0 0 0-1.5-1.5H3.75A1.5 1.5 0 0 0 2.25 6v12a1.5 1.5 0 0 0 1.5 1.5Zm10.5-11.25h.008v.008h-.008V8.25Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z"
                 />
               </svg>
 
-              <span className="font-medium text-[1.7rem]">Comments</span>
+              <span className="font-medium text-[1.7rem]">My Posts</span>
             </div>
           </Link>
-          <Link href={"/following"}>
+          <Link href={`/following/${userId}`}>
             <div className="sideItemDeActive active:sideItemActive">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
@@ -118,10 +118,10 @@ export default function Navbar({ userId }) {
                 />
               </svg>
 
-              <span className="font-medium text-[1.7rem]">Contacts</span>
+              <span className="font-medium text-[1.7rem]">Following</span>
             </div>
           </Link>
-          <Link href={"/"}>
+          <Link href={`/followers/${userId}`}>
             <div className="sideItemDeActive active:sideItemActive">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
@@ -134,14 +134,14 @@ export default function Navbar({ userId }) {
                 <path
                   strokeLinecap="round"
                   strokeLinejoin="round"
-                  d="M3.375 19.5h17.25m-17.25 0a1.125 1.125 0 0 1-1.125-1.125M3.375 19.5h1.5C5.496 19.5 6 18.996 6 18.375m-3.75 0V5.625m0 12.75v-1.5c0-.621.504-1.125 1.125-1.125m18.375 2.625V5.625m0 12.75c0 .621-.504 1.125-1.125 1.125m1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125m0 3.75h-1.5A1.125 1.125 0 0 1 18 18.375M20.625 4.5H3.375m17.25 0c.621 0 1.125.504 1.125 1.125M20.625 4.5h-1.5C18.504 4.5 18 5.004 18 5.625m3.75 0v1.5c0 .621-.504 1.125-1.125 1.125M3.375 4.5c-.621 0-1.125.504-1.125 1.125M3.375 4.5h1.5C5.496 4.5 6 5.004 6 5.625m-3.75 0v1.5c0 .621.504 1.125 1.125 1.125m0 0h1.5m-1.5 0c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125m1.5-3.75C5.496 8.25 6 7.746 6 7.125v-1.5M4.875 8.25C5.496 8.25 6 8.754 6 9.375v1.5m0-5.25v5.25m0-5.25C6 5.004 6.504 4.5 7.125 4.5h9.75c.621 0 1.125.504 1.125 1.125m1.125 2.625h1.5m-1.5 0A1.125 1.125 0 0 1 18 7.125v-1.5m1.125 2.625c-.621 0-1.125.504-1.125 1.125v1.5m2.625-2.625c.621 0 1.125.504 1.125 1.125v1.5c0 .621-.504 1.125-1.125 1.125M18 5.625v5.25M7.125 12h9.75m-9.75 0A1.125 1.125 0 0 1 6 10.875M7.125 12C6.504 12 6 12.504 6 13.125m0-2.25C6 11.496 5.496 12 4.875 12M18 10.875c0 .621-.504 1.125-1.125 1.125M18 10.875c0 .621.504 1.125 1.125 1.125m-2.25 0c.621 0 1.125.504 1.125 1.125m-12 5.25v-5.25m0 5.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125m-12 0v-1.5c0-.621-.504-1.125-1.125-1.125M18 18.375v-5.25m0 5.25v-1.5c0-.621.504-1.125 1.125-1.125M18 13.125v1.5c0 .621.504 1.125 1.125 1.125M18 13.125c0-.621.504-1.125 1.125-1.125M6 13.125v1.5c0 .621-.504 1.125-1.125 1.125M6 13.125C6 12.504 5.496 12 4.875 12m-1.5 0h1.5m-1.5 0c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125M19.125 12h1.5m0 0c.621 0 1.125.504 1.125 1.125v1.5c0 .621-.504 1.125-1.125 1.125m-17.25 0h1.5m14.25 0h1.5"
+                  d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z"
                 />
               </svg>
 
-              <span className="font-medium text-[1.7rem]">Stories</span>
+              <span className="font-medium text-[1.7rem]">Followers</span>
             </div>
           </Link>
-          <Link href={"/profiler"}>
+          <Link href={`/profiler/${userId}`}>
             <div className="sideItemDeActive active:sideItemActive">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
